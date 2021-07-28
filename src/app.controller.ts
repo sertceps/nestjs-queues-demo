aa826fa03433b2a1f48021feea859aa4b9155cb2
夏鹏程 <spectres@foxmail.com>
2021-07-28 18:26:57 +0800

    a simple provider/consumer

--- src/app.controller.ts
+++ src/app.controller.ts
@@ -1,12 +1,19 @@
-import { Controller, Get } from '@nestjs/common';
+import { Controller, Get, Query } from '@nestjs/common';
 import { AppService } from './app.service';
+import { MessageProducerService } from './message.producer.service';
 
 @Controller()
 export class AppController {
-  constructor(private readonly appService: AppService) {}
+  constructor(private readonly appService: AppService, private readonly messageProducerService: MessageProducerService) {}
 
   @Get()
   getHello(): string {
     return this.appService.getHello();
+  }
+
+  @Get('invoke-message')
+  async getInvokeMessage(@Query('message') message: string) {
+    this.messageProducerService.sendMessage(message);
+    return message;
   }
 }
--- src/app.module.ts
+++ src/app.module.ts
@@ -1,10 +1,24 @@
+import { BullModule } from '@nestjs/bull';
 import { Module } from '@nestjs/common';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
+import { MessageConsumer } from './message.consumer';
+import { MessageProducerService } from './message.producer.service';
 
+// https://www.learmoreseekmore.com/2021/04/guide-on-nestjs-queues.html
 @Module({
-  imports: [],
+  imports: [
+    BullModule.forRoot({
+      redis: {
+        host: 'localhost',
+        port: 6379
+      }
+    }),
+    BullModule.registerQueue({
+      name: 'message-queue'
+    })
+  ],
   controllers: [AppController],
-  providers: [AppService],
+  providers: [AppService, MessageProducerService, MessageConsumer]
 })
 export class AppModule {}
