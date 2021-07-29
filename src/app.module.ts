b5631d26c796f0c382098d10651ca0a6605dea61
夏鹏程 <spectres@foxmail.com>
2021-07-29 17:58:20 +0800

    Revert "Merge pull request #1 from sertceps/message"
    This reverts commit 928a83c62663dec83269bf067d93b94a0b4ba92a, reversing
    changes made to 2509b955ef2cce7dd7c861f4c92760a08067fd8f.

--- src/app.module.ts
+++ src/app.module.ts
@@ -5,6 +5,7 @@ import { AppController } from './app.controller';
 import { AppService } from './app.service';
 import { MessageConsumer } from './message-queue/message.consumer';
 import { MessageProducer } from './message-queue/message.producer';
+import { UserModule } from './user/user.module';
 
 @Module({
   imports: [
@@ -15,7 +16,9 @@ import { MessageProducer } from './message-queue/message.producer';
         port: 6379
       }
     }),
-    BullModule.registerQueue({ name: 'message-queue' })
+    BullModule.registerQueue({ name: 'message-queue' }),
+
+    UserModule
   ],
   controllers: [AppController],
   providers: [AppService, MessageProducer, MessageConsumer]
